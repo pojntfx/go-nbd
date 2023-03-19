37a481739f6951ea3ad22c70f6b8870b2a698783
Felicitas Pojtinger <felicitas@pojtinger.com>
2023-03-19 23:37:19 +0100

    feat: Implement export name, information requests and info replies for NBD client

--- cmd/nbd-client/main.go
+++ cmd/nbd-client/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"encoding/binary"
 	"errors"
 	"flag"
+	"io"
 	"log"
 	"net"
 	"os"
@@ -16,12 +18,15 @@ import (
 
 var (
 	errUnsupportedNetwork = errors.New("unsupported network")
+	errUnknownReply       = errors.New("unknown reply")
+	errUnknownInfo        = errors.New("unknown info")
 )
 
 const (
 	// See /usr/include/linux/nbd.h
-	NEGOTIATION_IOCTL_SET_SOCK = 43776
-	NEGOTIATION_IOCTL_DO_IT    = 43779
+	NEGOTIATION_IOCTL_SET_SOCK        = 43776
+	NEGOTIATION_IOCTL_SET_SIZE_BLOCKS = 43783
+	NEGOTIATION_IOCTL_DO_IT           = 43779
 
 	TRANSMISSION_IOCTL_DISCONNECT = 43784
 )
@@ -101,15 +106,70 @@ func main() {
 		panic(err)
 	}
 
-	// TODO: Implement `NEGOTIATION_ID_OPTION_GO` in userspace
-
-	var replyHeader protocol.NegotiationNewstyleHeader
-	if err := binary.Read(conn, binary.BigEndian, &replyHeader); err != nil {
+	exportName := "default"
+	if err := binary.Write(conn, binary.BigEndian, uint32(len(exportName))); err != nil { // Send export name length (uint32)
 		panic(err)
 	}
 
-	if replyHeader.OptionMagic != protocol.NEGOTIATION_MAGIC_REPLY {
-		panic(server.ErrInvalidMagic)
+	if _, err := conn.Write([]byte(exportName)); err != nil {
+		panic(err)
+	}
+
+	if err := binary.Write(conn, binary.BigEndian, uint16(0)); err != nil { // Send information request count (uint16)
+		panic(err)
+	}
+
+	size := uint64(0)
+	preferredBlockSize := uint32(1)
+
+n:
+	for {
+		var replyHeader protocol.NegotiationReplyHeader
+		if err := binary.Read(conn, binary.BigEndian, &replyHeader); err != nil {
+			panic(err)
+		}
+
+		if replyHeader.ReplyMagic != protocol.NEGOTIATION_MAGIC_REPLY {
+			panic(server.ErrInvalidMagic)
+		}
+
+		switch replyHeader.Type {
+		case protocol.NEGOTIATION_TYPE_REPLY_INFO:
+			infoRaw := make([]byte, replyHeader.Length)
+			if _, err := io.ReadFull(conn, infoRaw); err != nil {
+				panic(err)
+			}
+
+			var infoType uint16
+			if err := binary.Read(bytes.NewBuffer(infoRaw), binary.BigEndian, &infoType); err != nil {
+				panic(err)
+			}
+
+			switch infoType {
+			case protocol.NEGOTIATION_TYPE_INFO_EXPORT:
+				var info protocol.NegotiationReplyInfo
+				if err := binary.Read(bytes.NewBuffer(infoRaw), binary.BigEndian, &info); err != nil {
+					panic(err)
+				}
+
+				size = info.Size
+			default:
+				panic(errUnknownInfo)
+			}
+		case protocol.NEGOTIATION_TYPE_REPLY_ACK:
+			break n
+		default:
+			panic(errUnknownReply)
+		}
+	}
+
+	if _, _, err := syscall.Syscall(
+		syscall.SYS_IOCTL,
+		f.Fd(),
+		NEGOTIATION_IOCTL_SET_SIZE_BLOCKS,
+		uintptr(size/uint64(preferredBlockSize)),
+	); err != 0 {
+		panic(err)
 	}
 
 	sigCh := make(chan os.Signal, 1)
