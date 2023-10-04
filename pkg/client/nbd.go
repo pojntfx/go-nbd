eb21896f22d088def33040c05618d0308eb095e9
Felicitas Pojtinger <felicitas@pojtinger.com>
2023-10-05 00:30:55 +0200

    feat: Start adding multi-conn support on client (leads to `invalid argument` for now if `len(conns)` > 1.

--- pkg/client/nbd.go
+++ pkg/client/nbd.go
@@ -65,7 +65,7 @@ func negotiateNewstyle(conn net.Conn) error {
 	return nil
 }
 
-func Connect(conn net.Conn, device *os.File, options *Options) error {
+func Connect(conns []net.Conn, device *os.File, options *Options) error {
 	if options == nil {
 		options = &Options{}
 	}
@@ -78,240 +78,244 @@ func Connect(conn net.Conn, device *os.File, options *Options) error {
 		options.ReadyCheckPollInterval = time.Millisecond
 	}
 
-	var cfd uintptr
-	switch c := conn.(type) {
-	case *net.TCPConn:
-		file, err := c.File()
-		if err != nil {
-			return err
-		}
-
-		cfd = uintptr(file.Fd())
-	case *net.UnixConn:
-		file, err := c.File()
-		if err != nil {
-			return err
-		}
-
-		cfd = uintptr(file.Fd())
-	default:
-		return ErrUnsupportedNetwork
-	}
-
 	fatal := make(chan error)
-	if options.OnConnected != nil {
-		if options.ReadyCheckUdev {
-			udevConn := new(netlink.UEventConn)
-			if err := udevConn.Connect(netlink.UdevEvent); err != nil {
+	for i, conn := range conns {
+		var cfd uintptr
+		switch c := conn.(type) {
+		case *net.TCPConn:
+			file, err := c.File()
+			if err != nil {
 				return err
 			}
-			defer udevConn.Close()
 
-			var (
-				udevReadyCh = make(chan netlink.UEvent)
-				udevErrCh   = make(chan error)
-				udevQuit    = udevConn.Monitor(udevReadyCh, udevErrCh, &netlink.RuleDefinitions{
-					Rules: []netlink.RuleDefinition{
-						{
-							Env: map[string]string{
-								"DEVNAME": device.Name(),
+			cfd = uintptr(file.Fd())
+		case *net.UnixConn:
+			file, err := c.File()
+			if err != nil {
+				return err
+			}
+
+			cfd = uintptr(file.Fd())
+		default:
+			return ErrUnsupportedNetwork
+		}
+
+		if i == 0 && options.OnConnected != nil {
+			if options.ReadyCheckUdev {
+				udevConn := new(netlink.UEventConn)
+				if err := udevConn.Connect(netlink.UdevEvent); err != nil {
+					return err
+				}
+				defer udevConn.Close()
+
+				var (
+					udevReadyCh = make(chan netlink.UEvent)
+					udevErrCh   = make(chan error)
+					udevQuit    = udevConn.Monitor(udevReadyCh, udevErrCh, &netlink.RuleDefinitions{
+						Rules: []netlink.RuleDefinition{
+							{
+								Env: map[string]string{
+									"DEVNAME": device.Name(),
+								},
 							},
 						},
-					},
-				})
-			)
-			defer close(udevQuit)
+					})
+				)
+				defer close(udevQuit)
 
-			go func() {
-				select {
-				case <-udevReadyCh:
-					close(udevQuit)
+				go func() {
+					select {
+					case <-udevReadyCh:
+						close(udevQuit)
 
-					options.OnConnected()
-
-					return
-				case err := <-udevErrCh:
-					fatal <- err
-
-					return
-				}
-			}()
-		} else {
-			go func() {
-				sizeFile, err := os.Open(filepath.Join("/sys", "block", filepath.Base(device.Name()), "size"))
-				if err != nil {
-					fatal <- err
-
-					return
-				}
-				defer sizeFile.Close()
-
-				for {
-					if _, err := sizeFile.Seek(0, io.SeekStart); err != nil {
-						fatal <- err
-
-						return
-					}
-
-					rsize, err := io.ReadAll(sizeFile)
-					if err != nil {
-						fatal <- err
-
-						return
-					}
-
-					size, err := strconv.ParseInt(strings.TrimSpace(string(rsize)), 10, 64)
-					if err != nil {
-						fatal <- err
-
-						return
-					}
-
-					if size > 0 {
 						options.OnConnected()
 
 						return
+					case err := <-udevErrCh:
+						fatal <- err
+
+						return
 					}
+				}()
+			} else {
+				go func() {
+					sizeFile, err := os.Open(filepath.Join("/sys", "block", filepath.Base(device.Name()), "size"))
+					if err != nil {
+						fatal <- err
 
-					time.Sleep(options.ReadyCheckPollInterval)
-				}
-			}()
+						return
+					}
+					defer sizeFile.Close()
+
+					for {
+						if _, err := sizeFile.Seek(0, io.SeekStart); err != nil {
+							fatal <- err
+
+							return
+						}
+
+						rsize, err := io.ReadAll(sizeFile)
+						if err != nil {
+							fatal <- err
+
+							return
+						}
+
+						size, err := strconv.ParseInt(strings.TrimSpace(string(rsize)), 10, 64)
+						if err != nil {
+							fatal <- err
+
+							return
+						}
+
+						if size > 0 {
+							options.OnConnected()
+
+							return
+						}
+
+						time.Sleep(options.ReadyCheckPollInterval)
+					}
+				}()
+			}
 		}
-	}
 
-	if _, _, err := syscall.Syscall(
-		syscall.SYS_IOCTL,
-		device.Fd(),
-		ioctl.NEGOTIATION_IOCTL_SET_SOCK,
-		uintptr(cfd),
-	); err != 0 {
-		return err
-	}
-
-	if err := negotiateNewstyle(conn); err != nil {
-		return err
-	}
-
-	if err := binary.Write(conn, binary.BigEndian, protocol.NegotiationOptionHeader{
-		OptionMagic: protocol.NEGOTIATION_MAGIC_OPTION,
-		ID:          protocol.NEGOTIATION_ID_OPTION_GO,
-		Length:      0,
-	}); err != nil {
-		return err
-	}
-
-	exportName := []byte(options.ExportName)
-
-	if err := binary.Write(conn, binary.BigEndian, uint32(len(exportName))); err != nil {
-		return err
-	}
-
-	if _, err := conn.Write([]byte(exportName)); err != nil {
-		return err
-	}
-
-	if err := binary.Write(conn, binary.BigEndian, uint16(0)); err != nil { // Send information request count (uint16)
-		return err
-	}
-
-	size := uint64(0)
-	chosenBlockSize := uint32(1)
-
-n:
-	for {
-		var replyHeader protocol.NegotiationReplyHeader
-		if err := binary.Read(conn, binary.BigEndian, &replyHeader); err != nil {
+		if _, _, err := syscall.Syscall(
+			syscall.SYS_IOCTL,
+			device.Fd(),
+			ioctl.NEGOTIATION_IOCTL_SET_SOCK,
+			uintptr(cfd),
+		); err != 0 {
 			return err
 		}
 
-		if replyHeader.ReplyMagic != protocol.NEGOTIATION_MAGIC_REPLY {
-			return server.ErrInvalidMagic
+		if err := negotiateNewstyle(conn); err != nil {
+			return err
 		}
 
-		switch replyHeader.Type {
-		case protocol.NEGOTIATION_TYPE_REPLY_INFO:
-			infoRaw := make([]byte, replyHeader.Length)
-			if _, err := io.ReadFull(conn, infoRaw); err != nil {
+		if err := binary.Write(conn, binary.BigEndian, protocol.NegotiationOptionHeader{
+			OptionMagic: protocol.NEGOTIATION_MAGIC_OPTION,
+			ID:          protocol.NEGOTIATION_ID_OPTION_GO,
+			Length:      0,
+		}); err != nil {
+			return err
+		}
+
+		exportName := []byte(options.ExportName)
+
+		if err := binary.Write(conn, binary.BigEndian, uint32(len(exportName))); err != nil {
+			return err
+		}
+
+		if _, err := conn.Write([]byte(exportName)); err != nil {
+			return err
+		}
+
+		if err := binary.Write(conn, binary.BigEndian, uint16(0)); err != nil { // Send information request count (uint16)
+			return err
+		}
+
+		size := uint64(0)
+		chosenBlockSize := uint32(1)
+
+	n:
+		for {
+			var replyHeader protocol.NegotiationReplyHeader
+			if err := binary.Read(conn, binary.BigEndian, &replyHeader); err != nil {
 				return err
 			}
 
-			var infoType uint16
-			if err := binary.Read(bytes.NewBuffer(infoRaw), binary.BigEndian, &infoType); err != nil {
-				return err
+			if replyHeader.ReplyMagic != protocol.NEGOTIATION_MAGIC_REPLY {
+				return server.ErrInvalidMagic
 			}
 
-			switch infoType {
-			case protocol.NEGOTIATION_TYPE_INFO_EXPORT:
-				var info protocol.NegotiationReplyInfo
-				if err := binary.Read(bytes.NewBuffer(infoRaw), binary.BigEndian, &info); err != nil {
+			switch replyHeader.Type {
+			case protocol.NEGOTIATION_TYPE_REPLY_INFO:
+				infoRaw := make([]byte, replyHeader.Length)
+				if _, err := io.ReadFull(conn, infoRaw); err != nil {
 					return err
 				}
 
-				size = info.Size
-			case protocol.NEGOTIATION_TYPE_INFO_NAME:
-				// Discard export name
-			case protocol.NEGOTIATION_TYPE_INFO_DESCRIPTION:
-				// Discard export description
-			case protocol.NEGOTIATION_TYPE_INFO_BLOCKSIZE:
-				var info protocol.NegotiationReplyBlockSize
-				if err := binary.Read(bytes.NewBuffer(infoRaw), binary.BigEndian, &info); err != nil {
+				var infoType uint16
+				if err := binary.Read(bytes.NewBuffer(infoRaw), binary.BigEndian, &infoType); err != nil {
 					return err
 				}
 
-				if options.BlockSize == 0 {
-					chosenBlockSize = info.PreferredBlockSize
-				} else if options.BlockSize >= info.MinimumBlockSize && options.BlockSize <= info.MaximumBlockSize {
-					chosenBlockSize = options.BlockSize
-				} else {
-					return ErrUnsupportedServerBlockSize
-				}
+				switch infoType {
+				case protocol.NEGOTIATION_TYPE_INFO_EXPORT:
+					var info protocol.NegotiationReplyInfo
+					if err := binary.Read(bytes.NewBuffer(infoRaw), binary.BigEndian, &info); err != nil {
+						return err
+					}
 
-				if chosenBlockSize > MaximumBlockSize {
-					return ErrMaximumBlockSize
-				} else if chosenBlockSize < MinimumBlockSize {
-					return ErrMinimumBlockSize
-				}
+					size = info.Size
+				case protocol.NEGOTIATION_TYPE_INFO_NAME:
+					// Discard export name
+				case protocol.NEGOTIATION_TYPE_INFO_DESCRIPTION:
+					// Discard export description
+				case protocol.NEGOTIATION_TYPE_INFO_BLOCKSIZE:
+					var info protocol.NegotiationReplyBlockSize
+					if err := binary.Read(bytes.NewBuffer(infoRaw), binary.BigEndian, &info); err != nil {
+						return err
+					}
 
-				if !((chosenBlockSize > 0) && ((chosenBlockSize & (chosenBlockSize - 1)) == 0)) {
-					return ErrBlockSizeNotPowerOfTwo
+					if options.BlockSize == 0 {
+						chosenBlockSize = info.PreferredBlockSize
+					} else if options.BlockSize >= info.MinimumBlockSize && options.BlockSize <= info.MaximumBlockSize {
+						chosenBlockSize = options.BlockSize
+					} else {
+						return ErrUnsupportedServerBlockSize
+					}
+
+					if chosenBlockSize > MaximumBlockSize {
+						return ErrMaximumBlockSize
+					} else if chosenBlockSize < MinimumBlockSize {
+						return ErrMinimumBlockSize
+					}
+
+					if !((chosenBlockSize > 0) && ((chosenBlockSize & (chosenBlockSize - 1)) == 0)) {
+						return ErrBlockSizeNotPowerOfTwo
+					}
+				default:
+					return ErrUnknownInfo
 				}
+			case protocol.NEGOTIATION_TYPE_REPLY_ACK:
+				break n
+			case protocol.NEGOTIATION_TYPE_REPLY_ERR_UNKNOWN:
+				return ErrUnknownErr
 			default:
-				return ErrUnknownInfo
+				return ErrUnknownReply
 			}
-		case protocol.NEGOTIATION_TYPE_REPLY_ACK:
-			break n
-		case protocol.NEGOTIATION_TYPE_REPLY_ERR_UNKNOWN:
-			return ErrUnknownErr
-		default:
-			return ErrUnknownReply
 		}
-	}
 
-	if _, _, err := syscall.Syscall(
-		syscall.SYS_IOCTL,
-		device.Fd(),
-		ioctl.NEGOTIATION_IOCTL_SET_BLOCKSIZE,
-		uintptr(chosenBlockSize),
-	); err != 0 {
-		return err
-	}
+		if i == 0 {
+			if _, _, err := syscall.Syscall(
+				syscall.SYS_IOCTL,
+				device.Fd(),
+				ioctl.NEGOTIATION_IOCTL_SET_BLOCKSIZE,
+				uintptr(chosenBlockSize),
+			); err != 0 {
+				return err
+			}
 
-	if _, _, err := syscall.Syscall(
-		syscall.SYS_IOCTL,
-		device.Fd(),
-		ioctl.NEGOTIATION_IOCTL_SET_SIZE_BLOCKS,
-		uintptr(size/uint64(chosenBlockSize)),
-	); err != 0 {
-		return err
-	}
+			if _, _, err := syscall.Syscall(
+				syscall.SYS_IOCTL,
+				device.Fd(),
+				ioctl.NEGOTIATION_IOCTL_SET_SIZE_BLOCKS,
+				uintptr(size/uint64(chosenBlockSize)),
+			); err != 0 {
+				return err
+			}
 
-	if _, _, err := syscall.Syscall(
-		syscall.SYS_IOCTL,
-		device.Fd(),
-		ioctl.NEGOTIATION_IOCTL_SET_TIMEOUT,
-		uintptr(options.Timeout),
-	); err != 0 {
-		return err
+			if _, _, err := syscall.Syscall(
+				syscall.SYS_IOCTL,
+				device.Fd(),
+				ioctl.NEGOTIATION_IOCTL_SET_TIMEOUT,
+				uintptr(options.Timeout),
+			); err != 0 {
+				return err
+			}
+		}
 	}
 
 	go func() {
